3ffbdb7046115a4aa2adf171f98774c979298d78
ErikCald <erikcaell@gmail.com>
2025-10-30 12:19:58 -0400

    Reset git history

--- src/main/java/org/example/ProjectStatus.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public enum ProjectStatus {
-    OPEN,      // Available for applications
-    FULL,      // Application limit reached
-    ARCHIVED   // No longer offered, but kept for history
-}
