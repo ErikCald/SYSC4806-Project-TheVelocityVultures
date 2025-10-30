695c6a90d161b762cfb1504d605364734127c142
Lakshman Chelliah <lakshman.chelliah@gmail.com>
2025-10-30 12:39:51 -0400

    (feat): First commit - Baseline Implementation of ProjectTopic, Students, Professor and supporting enums

---
+++ src/main/java/org/example/ProjectStatus.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum ProjectStatus {
+    OPEN,      // Available for applications
+    FULL,      // Application limit reached
+    ARCHIVED   // No longer offered, but kept for history
+}
